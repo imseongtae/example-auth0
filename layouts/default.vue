48316d7e084d867bf8282fb60ea2f9cf3e25f520
imseongtae <dev.haemil@gmail.com>
2020-11-26 13:24:20 +0900

    feat: Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <fork-this />
+    <main class="main" role="main">
+      <navbar />
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+import ForkThis from '@/components/ForkThis.vue';
+
+export default {
+  components: {
+    Navbar,
+    ForkThis,
+  },
+};
+</script>
+
+<style scoped>
+.app {
+  height: 100vh;
+  width: 100vw;
+}
+
+.main {
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 30px;
+}
+</style>
