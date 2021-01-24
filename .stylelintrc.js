7ec8910514cd97cbdba658b4f0e05ea3737426d1
imseongtae <dev.haemil@gmail.com>
2021-01-24 21:25:24 +0900

    chore: Add stylelint.js

---
+++ .stylelintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  extends: ['stylelint-config-standard'],
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  rules: {
+    'at-rule-no-unknown': null,
+    // 'scss/at-rule-no-unknown': true,
+    // 'order/properties-alphabetical-order': true,
+    'no-empty-source': null,
+    'rule-empty-line-before': null,
+    'selector-list-comma-newline-after': null,
+    'no-descending-specificity': null,
+    // 
+    'declaration-block-trailing-semicolon': null,    
+    'declaration-empty-line-before': null,
+    'at-rule-empty-line-before': 'never',
+        
+    // 'scss/at-rule-no-unknown': null
+  },
+};
